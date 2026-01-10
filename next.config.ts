ef1b3a0e3b1de0448405f7c64851e760db1a7a8e
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-01-10 18:33:05 +0200

    chore: init Next.js project with typescript and pages router

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  i18n:{
+    locales: ["ar", "en"],
+    defaultLocale: "en",
+  }
 };
 
 export default nextConfig;
