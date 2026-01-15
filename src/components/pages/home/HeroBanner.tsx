63b9a78ce1d36b33b4c7a77a43f890f516c22de0
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-01-15 23:45:23 +0200

    fix some ui issues

--- src/components/pages/home/HeroBanner.tsx
+++ src/components/pages/home/HeroBanner.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from '@/styles/pages/Home.module.css'
+import { getImagePath } from '@/utils/utils'
 
 const HeroBanner = () => {
   return (
     <div className={`${styles["hero-banner"]}`}>
-        <Image src="/assets/images/slider-1.jpg" alt="Slider-1" fill style={{objectFit: "cover"}}/>
+        <Image src={getImagePath("slider-1.jpg")} alt="Slider-1" fill style={{objectFit: "cover"}}/>
     </div>
   )
 }
